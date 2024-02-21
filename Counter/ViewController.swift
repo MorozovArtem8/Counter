03b9fb2cae3e91c73f91870b209a575e19e9cff0
MorozovArtem8 <Vjhjpjaa32@yandex.ru>
2024-02-21 19:42:31 +0300

    commit sprint03

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,19 +1,53 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by Artem Morozov on 21.02.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: IBOutle
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var infoTextField: UITextView!
+    
+    private var labelCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        infoTextField.isSelectable = false
+        labelUpdate()
     }
-
-
+    
+    //MARK: IBAction
+    @IBAction func addButtondTapped(_ sender: UIButton) {
+        labelCounter += 1
+        labelUpdate()
+        infoTextField.text += "\n\(currentDateString()): Значение изменено на +1"
+    }
+    
+    @IBAction func subtractButtondTapped(_ sender: UIButton) {
+        if labelCounter > 0 {
+            labelCounter -= 1
+            labelUpdate()
+            infoTextField.text += "\n\(currentDateString()): Значение изменено на -1"
+        }else {
+            infoTextField.text += "\n\(currentDateString()): Попытка уменьшить значение счётчика ниже 0"
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        labelCounter = 0
+        labelUpdate()
+        infoTextField.text += "\n\(currentDateString()): Значение сброшено"
+    }
+    
+    private func currentDateString() -> String {
+        let dateFormater = DateFormatter()
+        dateFormater.dateStyle = .short
+        dateFormater.timeStyle = .short
+        dateFormater.locale = Locale(identifier: "ru_RU")
+        let date = Date()
+        
+        return String(dateFormater.string(from: date))
+    }
+    
+    private func labelUpdate() {
+        counterLabel.text = "Значение счётчика: \(labelCounter)"
+    }
 }
 

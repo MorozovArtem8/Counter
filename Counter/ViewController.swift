7b04a4fd180541ca5a99122278f96bc13a362935
MorozovArtem8 <Vjhjpjaa32@yandex.ru>
2024-02-22 19:00:30 +0300

    fix

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
-    //MARK: IBOutle
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var infoTextField: UITextView!
+final class ViewController: UIViewController {
+    //MARK: IBOutlet
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var infoTextField: UITextView!
     
-    private var labelCounter = 0
+    private var labelCounter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
     }
     
     //MARK: IBAction
-    @IBAction func addButtondTapped(_ sender: UIButton) {
+    @IBAction private func addButtonTapped(_ sender: UIButton) {
         labelCounter += 1
         labelUpdate()
         infoTextField.text += "\n\(currentDateString()): Значение изменено на +1"
     }
     
-    @IBAction func subtractButtondTapped(_ sender: UIButton) {
+    @IBAction private func subtractButtonTapped(_ sender: UIButton) {
         if labelCounter > 0 {
             labelCounter -= 1
             labelUpdate()
@@ -30,20 +30,20 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    @IBAction private func resetButtonTapped(_ sender: UIButton) {
         labelCounter = 0
         labelUpdate()
         infoTextField.text += "\n\(currentDateString()): Значение сброшено"
     }
     
     private func currentDateString() -> String {
-        let dateFormater = DateFormatter()
-        dateFormater.dateStyle = .short
-        dateFormater.timeStyle = .short
-        dateFormater.locale = Locale(identifier: "ru_RU")
+        let dateFormate = DateFormatter()
+        dateFormate.dateStyle = .short
+        dateFormate.timeStyle = .short
+        dateFormate.locale = Locale(identifier: "ru_RU")
         let date = Date()
         
-        return String(dateFormater.string(from: date))
+        return String(dateFormate.string(from: date))
     }
     
     private func labelUpdate() {
